1fef986987d31e17561231dd8842d545afdda607
Pistipisti94 <pistipisti57@gmail.com>
2023-11-13 13:28:32 +0100

    Add project files.

---
+++ WindowsFormsAppok/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace WindowsFormsAppok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gombBetoltes = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxlista = new System.Windows.Forms.ListBox();
+            this.forrasfajlneveinput = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.gombTeruletekAtlaga = new System.Windows.Forms.Button();
+            this.gombMegszamolas = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gombKiiras = new System.Windows.Forms.Button();
+            this.gombMinMaxKivalasztas = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.gombKereses = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gombBetoltes
+            // 
+            this.gombBetoltes.Location = new System.Drawing.Point(12, 48);
+            this.gombBetoltes.Name = "gombBetoltes";
+            this.gombBetoltes.Size = new System.Drawing.Size(190, 23);
+            this.gombBetoltes.TabIndex = 0;
+            this.gombBetoltes.Text = "Betöltés";
+            this.gombBetoltes.UseVisualStyleBackColor = true;
+            this.gombBetoltes.Click += new System.EventHandler(this.gombBetoltes_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Minimum",
+            "Maximum"});
+            this.comboBox1.Location = new System.Drawing.Point(208, 200);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(190, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Minimum";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Forrásfájl neve:";
+            // 
+            // listBoxlista
+            // 
+            this.listBoxlista.FormattingEnabled = true;
+            this.listBoxlista.Location = new System.Drawing.Point(12, 77);
+            this.listBoxlista.Name = "listBoxlista";
+            this.listBoxlista.Size = new System.Drawing.Size(190, 251);
+            this.listBoxlista.TabIndex = 3;
+            // 
+            // forrasfajlneveinput
+            // 
+            this.forrasfajlneveinput.Location = new System.Drawing.Point(12, 26);
+            this.forrasfajlneveinput.Name = "forrasfajlneveinput";
+            this.forrasfajlneveinput.Size = new System.Drawing.Size(190, 20);
+            this.forrasfajlneveinput.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 17);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(163, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "100.000-nél nagyobb területű";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(208, 308);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(164, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Találatok kijelölése a listában";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(208, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(190, 62);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mit számoljunk?";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 39);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(150, 17);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "legfeljebb 100.000 területű";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // gombTeruletekAtlaga
+            // 
+            this.gombTeruletekAtlaga.Location = new System.Drawing.Point(12, 334);
+            this.gombTeruletekAtlaga.Name = "gombTeruletekAtlaga";
+            this.gombTeruletekAtlaga.Size = new System.Drawing.Size(190, 23);
+            this.gombTeruletekAtlaga.TabIndex = 0;
+            this.gombTeruletekAtlaga.Text = "Területek átlaga";
+            this.gombTeruletekAtlaga.UseVisualStyleBackColor = true;
+            // 
+            // gombMegszamolas
+            // 
+            this.gombMegszamolas.Location = new System.Drawing.Point(208, 77);
+            this.gombMegszamolas.Name = "gombMegszamolas";
+            this.gombMegszamolas.Size = new System.Drawing.Size(190, 23);
+            this.gombMegszamolas.TabIndex = 0;
+            this.gombMegszamolas.Text = "Megszámolás";
+            this.gombMegszamolas.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Eredményfájl neve:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(208, 119);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(208, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Minimum vagy maximum?";
+            // 
+            // gombKiiras
+            // 
+            this.gombKiiras.Location = new System.Drawing.Point(208, 145);
+            this.gombKiiras.Name = "gombKiiras";
+            this.gombKiiras.Size = new System.Drawing.Size(190, 23);
+            this.gombKiiras.TabIndex = 0;
+            this.gombKiiras.Text = "Kiírás";
+            this.gombKiiras.UseVisualStyleBackColor = true;
+            // 
+            // gombMinMaxKivalasztas
+            // 
+            this.gombMinMaxKivalasztas.Location = new System.Drawing.Point(208, 227);
+            this.gombMinMaxKivalasztas.Name = "gombMinMaxKivalasztas";
+            this.gombMinMaxKivalasztas.Size = new System.Drawing.Size(190, 23);
+            this.gombMinMaxKivalasztas.TabIndex = 0;
+            this.gombMinMaxKivalasztas.Text = "Minimum / Maximum kiválasztása";
+            this.gombMinMaxKivalasztas.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(208, 266);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Ország keresése:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(208, 282);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(190, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // gombKereses
+            // 
+            this.gombKereses.Location = new System.Drawing.Point(208, 334);
+            this.gombKereses.Name = "gombKereses";
+            this.gombKereses.Size = new System.Drawing.Size(190, 23);
+            this.gombKereses.TabIndex = 0;
+            this.gombKereses.Text = "Keresés";
+            this.gombKereses.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 373);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.forrasfajlneveinput);
+            this.Controls.Add(this.listBoxlista);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.gombKereses);
+            this.Controls.Add(this.gombMinMaxKivalasztas);
+            this.Controls.Add(this.gombKiiras);
+            this.Controls.Add(this.gombMegszamolas);
+            this.Controls.Add(this.gombTeruletekAtlaga);
+            this.Controls.Add(this.gombBetoltes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Példa Windows Form alkalmazás";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button gombBetoltes;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxlista;
+        private System.Windows.Forms.TextBox forrasfajlneveinput;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button gombTeruletekAtlaga;
+        private System.Windows.Forms.Button gombMegszamolas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button gombKiiras;
+        private System.Windows.Forms.Button gombMinMaxKivalasztas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button gombKereses;
+    }
+}
+
