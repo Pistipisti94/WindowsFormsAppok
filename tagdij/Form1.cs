c012ccb9b74eb9964ffacdfba6abca3c6958d051
Pistipisti94 <pistipisti57@gmail.com>
2023-11-27 10:06:36 +0100

    connection

--- tagdij/Form1.cs
+++ tagdij/Form1.cs
@@ -20,11 +20,6 @@ namespace tagdij
 
         private void Tag_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
             tagokBetoltese();
         }
 
@@ -36,6 +31,25 @@ namespace tagdij
             sb.UserID = "root";
             sb.Password = "";
             sb.Database = "tagdij";
+            MySqlConnection connection = new MySqlConnection(sb.ConnectionString);
+            try
+            {
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT `azon`,`nev`,`szulev`,`irszam`,`orsz` FROM `ugyfel` WHERE 1 ORDER BY nev";
+                using (MySqlDataReader dr = command.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        listBoxTagok.Items.Add(dr.GetString("nev"));
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                Environment.Exit(0);
+            }
         }
     }
 }
