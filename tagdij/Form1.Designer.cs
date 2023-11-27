86ac594bbac1538cb74afc7e01fd5a8d15602a38
Pistipisti94 <pistipisti57@gmail.com>
2023-11-27 09:43:06 +0100

    database connection winform

---
+++ tagdij/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace tagdij
+{
+    partial class Tag
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tag));
+            this.listBoxTagok = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_azon = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_nev = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_orsz = new System.Windows.Forms.TextBox();
+            this.numericUpDown_irszam = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_szulev = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_irszam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_szulev)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxTagok
+            // 
+            this.listBoxTagok.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            resources.ApplyResources(this.listBoxTagok, "listBoxTagok");
+            this.listBoxTagok.FormattingEnabled = true;
+            this.listBoxTagok.Name = "listBoxTagok";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown_szulev);
+            this.groupBox1.Controls.Add(this.numericUpDown_irszam);
+            this.groupBox1.Controls.Add(this.textBox_orsz);
+            this.groupBox1.Controls.Add(this.textBox_nev);
+            this.groupBox1.Controls.Add(this.textBox_azon);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // textBox_azon
+            // 
+            resources.ApplyResources(this.textBox_azon, "textBox_azon");
+            this.textBox_azon.Name = "textBox_azon";
+            this.textBox_azon.ReadOnly = true;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // textBox_nev
+            // 
+            resources.ApplyResources(this.textBox_nev, "textBox_nev");
+            this.textBox_nev.Name = "textBox_nev";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // textBox_orsz
+            // 
+            resources.ApplyResources(this.textBox_orsz, "textBox_orsz");
+            this.textBox_orsz.Name = "textBox_orsz";
+            // 
+            // numericUpDown_irszam
+            // 
+            this.numericUpDown_irszam.Increment = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            resources.ApplyResources(this.numericUpDown_irszam, "numericUpDown_irszam");
+            this.numericUpDown_irszam.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numericUpDown_irszam.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown_irszam.Name = "numericUpDown_irszam";
+            this.numericUpDown_irszam.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown_szulev
+            // 
+            this.numericUpDown_szulev.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            resources.ApplyResources(this.numericUpDown_szulev, "numericUpDown_szulev");
+            this.numericUpDown_szulev.Maximum = new decimal(new int[] {
+            2023,
+            0,
+            0,
+            0});
+            this.numericUpDown_szulev.Minimum = new decimal(new int[] {
+            1900,
+            0,
+            0,
+            0});
+            this.numericUpDown_szulev.Name = "numericUpDown_szulev";
+            this.numericUpDown_szulev.Value = new decimal(new int[] {
+            1950,
+            0,
+            0,
+            0});
+            // 
+            // Tag
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBoxTagok);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Tag";
+            this.Load += new System.EventHandler(this.Tag_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_irszam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_szulev)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxTagok;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_orsz;
+        private System.Windows.Forms.TextBox textBox_nev;
+        private System.Windows.Forms.TextBox textBox_azon;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown_irszam;
+        private System.Windows.Forms.NumericUpDown numericUpDown_szulev;
+    }
+}
+
---
+++ tagdij/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace tagdij
+{
+    public partial class Tag : Form
+    {
+        public Tag()
+        {
+            InitializeComponent();
+        }
+
+        private void Tag_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            tagokBetoltese();
+        }
+
+        private void tagokBetoltese()
+        {
+            MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder();
+            sb.Clear();
+            sb.Server = "localhost";
+            sb.UserID = "root";
+            sb.Password = "";
+            sb.Database = "tagdij";
+        }
+    }
+}
